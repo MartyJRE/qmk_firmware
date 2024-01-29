f0fd9d4b3c26e2c54ac7d8acfe86c3e64b517290
Martin Pražák <martin.prazak@lutherone.com>
2024-01-29 12:37:05 +0100

    animations

--- keyboards/boardsource/unicorne/unicorne.c
+++ keyboards/boardsource/unicorne/unicorne.c
@@ -3,35 +3,42 @@
 #include "unicorne.h"
 
 #ifdef OLED_ENABLE
+#define ANIM_INVERT false
+#define ANIM_RENDER_WPM false
+#define FAST_TYPE_WPM 40
+#include "music-bars.c"
+
 oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
     if (!is_keyboard_master()) {
-        return OLED_ROTATION_180;
+        return OLED_ROTATION_0;
     }
     return rotation;
 }
 
 bool oled_task_kb(void) {
-    if (!oled_task_user()) {
-        return false;
-    }
-    if (is_keyboard_master()) {
-        switch (get_highest_layer(layer_state)) {
-            case 0:
-                oled_write_raw(layer_zero, sizeof(layer_zero));
-                break;
-            case 1:
-                oled_write_raw(layer_one, sizeof(layer_one));
-                break;
-            case 2:
-                oled_write_raw(layer_two, sizeof(layer_two));
-                break;
-            case 3:
-                oled_write_raw(layer_three, sizeof(layer_three));
-                break;
-        }
-    } else {
-        oled_write_raw(logo, sizeof(logo));
-    }
+    //    if (!oled_task_user()) {
+    //        return false;
+    //    }
+    //    if (is_keyboard_master()) {
+    //        switch (get_highest_layer(layer_state)) {
+    //            case 0:
+    //                oled_write_raw(layer_zero, sizeof(layer_zero));
+    //                break;
+    //            case 1:
+    //                oled_write_raw(layer_one, sizeof(layer_one));
+    //                break;
+    //            case 2:
+    //                oled_write_raw(layer_two, sizeof(layer_two));
+    //                break;
+    //            case 3:
+    //                oled_write_raw(layer_three, sizeof(layer_three));
+    //                break;
+    //        }
+    //    } else {
+    //        oled_write_raw(logo, sizeof(logo));
+    //    }
+    oled_render_anim();
+
     return false;
 }
 #endif
